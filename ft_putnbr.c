0221c383d6e0b936db0205e7d1075537389a79e5
Jibran Kalia <jibran.kalia@gmail.com>
2017-02-13 15:16:12 -0800

    Completed ft_putchar and ft_putnbr

---
+++ ft_putnbr.c
@@ -0,0 +1,21 @@
+#include <nbr.h> 
+#include <chr.h> 
+
+void	ft_putnbr(int n)
+{
+	long nb;
+	nb = n;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = - nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
