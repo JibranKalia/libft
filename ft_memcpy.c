b64582f472b7e516d7411faa8a7e6877b37bf7a6
Jibran Kalia <jibran.kalia@gmail.com>
2017-02-14 20:26:50 -0800

    Futher Change to memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,16 +1,14 @@
 #include "libft.h" 
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
-	size_t i;
 	char	*csrc;
 	char	*cdst;
 	
-	i = -1;
 	csrc = (char*)src; 
 	cdst = (char*)dst;
 
-	while (++i < n)
-		cdst[i] = csrc[i];
+	while (len-- > 0)
+		*cdst++ = *csrc++;
 	return (cdst);
 }
