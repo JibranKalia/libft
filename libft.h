486ead668b5974a335f4f199351385b6b68070f8
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-03-12 11:05:40 -0700

    Changing the CHKS on libft.h. TEST

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 17:58:53 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/11 10:23:49 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/12 11:05:24 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,21 @@
 # define LIBFT_H
 # include <string.h>
 
+/**
 # define CHK(a, b) do{if(a){return(b);}}while(0)
 # define CHK1(a, b, c) do{if(a){b; return(c);}}while(0)
 # define CHK2(a, b, c, d) do{if(a){b; c; return(d);}}while(0)
 # define CHK3(a, b, c, d, e) do{if(a){b; c; d; return(e);}}while(0)
 # define CHK4(a, b, c, d, e, f) do{if(a){b; c; d; e; return(f);}}while(0)
+**/
+
+
+# define CHK(a, b) if(1){if(a){return(b);}}
+# define CHK1(a, b, c) if(1){if(a){b; return(c);}}
+# define CHK2(a, b, c, d) if(1){if(a){b; c; return(d);}}
+# define CHK3(a, b, c, d, e) if(1){if(a){b; c; d; return(e);}}
+# define CHK4(a, b, c, d, e, f) if(1){if(a){b; c; d; e; return(f);}}
+
 # define ISSPACE(a) ((a >= 9 && a <= 13) || (a == 32))
 # define ISDIGIT(a) (a >= 48 && a <= 57)
 
