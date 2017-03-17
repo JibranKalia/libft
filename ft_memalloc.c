1061e20838bbcac96a258a4785228fa7ce4dbee2
Jibran Kalia <jkalia@e1z2r9p33.42.us.org>
2017-03-17 15:59:02 -0700

    Simplifed ft_mmealloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 17:19:19 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/03 17:19:21 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/17 15:56:05 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 
 void	*ft_memalloc(size_t size)
 {
-	void			*tmp1;
-	unsigned char	*tmp2;
+	void			*ret;
 
 	CHK(size == 0, 0);
-	CHK((tmp1 = malloc(size)) == 0, 0);
-	tmp2 = (unsigned char*)tmp1;
-	while (size--)
-		*tmp2++ = 0;
-	return (tmp1);
+	CHK((ret = malloc(size)) == 0, 0);
+	ft_bzero(ret, size);
+	return (ret);
 }
