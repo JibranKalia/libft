1bfebb72644ab59ca1168727cf03dc18363922a9
Jibran Kalia <jkalia@e1z2r7p8.42.us.org>
2017-03-18 23:27:03 -0700

    Null protected ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 17:43:41 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/03 17:43:55 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/18 22:35:53 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_strchr(const char *src, int c)
 {
 	unsigned char	to_find;
 
+	CHK(!src, 0);
 	to_find = (unsigned char)c;
 	while (*src != '\0' && *src != to_find)
 		src++;
