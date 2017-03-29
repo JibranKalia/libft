1078ad6c7ab6ea79f7781e51a737217548ae511a
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-03-29 15:02:41 -0700

    if newsize is 0 in realloc free ptr

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/17 15:52:14 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/22 16:57:58 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/29 15:00:48 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	*ft_realloc(void *src, size_t srcsize, size_t newsize)
 	void	*res;
 	size_t	cpylen;
 
+	CHK(newsize == 0, free(src), 0);
 	cpylen = (srcsize < newsize) ? srcsize : newsize;
 	CHK(!src, 0);
 	CHK((res = ft_memalloc(newsize)) == 0, 0);
