ecfe60c91a639e679c5e6aefe63999a3260460cb
Jibran Kalia <jkalia@student.42.us.org>
2017-04-22 13:21:19 -0700

    Fixed issue with strjoinf

--- src/str/ft_strjoinf.c
+++ src/str/ft_strjoinf.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/23 20:37:15 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/22 13:09:12 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/04/22 13:21:05 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ char	*ft_strjoinf(char *src1, char *src2, int d)
 {
 	char	*ret;
 	char	*dst;
+	char	*s1;
+	char	*s2;
 	size_t	len;
 
+	s1 = src1;
+	s2 = src2;
 	CHK(src1 == 0 || src2 == 0, 0);
 	CHK(d == 0, 0);
 	len = ft_strlen(src1);
@@ -31,13 +35,13 @@ char	*ft_strjoinf(char *src1, char *src2, int d)
 		*dst++ = *src2++;
 	*dst = '\0';
 	if (d == 1)
-		free(src1);
+		free(s1);
 	else if (d == 2)
-		free(src2);
+		free(s2);
 	else if (d == 3)
 	{
-		free(src1);
-		free(src2);
+		free(s1);
+		free(s2);
 	}
 	return (ret);
 }
