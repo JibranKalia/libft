a866014fc6bd4744ba0f174e0dfd93c5799ff0b4
Jibran Kalia <jkalia@student.42.us.org>
2017-05-21 16:52:32 -0700

    Better checking for null in FT_COUNTWORDS

--- src/str/ft_countwords.c
+++ src/str/ft_countwords.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 22:29:24 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/21 22:30:52 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/05/21 16:51:48 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		ft_countwords(char const *src, char c)
 	int		i;
 	int		wrd_flag;
 
+	CHK(src == 0 || *src == 0, 0);
 	wrd_flag = 0;
 	i = 0;
 	while (*src)
