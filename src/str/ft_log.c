bdbd1d04574dba9c3fbc35fcfa99dc6a26c41311
Jibran Kalia <jkalia@student.42.us.org>
2017-05-25 16:20:58 -0700

    Fixed issue in ft_log

--- src/str/ft_log.c
+++ src/str/ft_log.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/30 08:32:33 by jkalia            #+#    #+#             */
-/*   Updated: 2017/04/30 12:07:19 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/05/25 16:20:38 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,12 @@ int		ft_log_err(char *file, int line, char *message, ...)
 
 int		ft_log_debug(char *file, int line, char *message, ...)
 {
-	int		fd;
-	char	*str_errno;
-	va_list	ap;
+	int			fd;
+	va_list		ap;
 
-	str_errno = errno == 0 ? "None" : strerror(errno);
 	fd = fileno(stderr);
 	va_start(ap, message);
-	ft_dprintf(fd, "%{green}[DEBUG] %s:%d: ", file, line, str_errno);
+	ft_dprintf(fd, "%{green}[DEBUG] %s:%d: ", file, line);
 	ft_vdprintf(fd, message, ap);
 	ft_dprintf(fd, "%{eoc}\n");
 	return (0);
